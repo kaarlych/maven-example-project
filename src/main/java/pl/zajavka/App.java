817b6adb62825aa512ba7d58ec58355bf223ac9b
Karol Janowski <karoljanowski@yahoo.com>
2025-03-14 12:13:42 +0100

    update

--- src/main/java/pl/zajavka/App.java
+++ src/main/java/pl/zajavka/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("dupa sraka");
         System.out.println("dupa sraka");
         System.out.println("dupa sraka");
+        System.out.println("dupa sraka");
     }
 }
