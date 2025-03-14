16395da892f98196548cefaec2df352ba5bcf231
Karol Janowski <karoljanowski@yahoo.com>
2025-03-14 12:10:17 +0100

    update

--- src/main/java/pl/zajavka/App.java
+++ src/main/java/pl/zajavka/App.java
@@ -6,5 +6,6 @@ package pl.zajavka;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("dupa sraka");
     }
 }
